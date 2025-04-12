b4e919d40c4b278a695f770ac09346e8de72db1e
AnuragYadav404 <124185505+AnuragYadav404@users.noreply.github.com>
2025-04-12 13:56:48 +0200

    Integrates Tailwind and Creates Boilerplate Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Certification from "./components/Certification"
+import Introduction from "./components/Introduction"
+import NavigationBar from "./components/NavigationBar"
+import PreviousWork from "./components/PreviousWork"
+import Project from "./components/Project"
+import Publication from "./components/Publication"
+import Skill from "./components/Skill"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavigationBar />
+      <Introduction />
+      <Skill />
+      <PreviousWork />
+      <Publication />
+      <Project />
+      <Certification />
     </>
   )
 }
