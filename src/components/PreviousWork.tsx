ab251df1e1a5ff02b8f4b0600948b5499f9e6bcf
AnuragYadav404 <124185505+AnuragYadav404@users.noreply.github.com>
2025-04-13 13:58:07 +0200

    Implements styling for previous work experience

--- src/components/PreviousWork.tsx
+++ src/components/PreviousWork.tsx
@@ -12,22 +12,27 @@ import { previousWorks } from "../data/previousWork"
 
 export default function PreviousWork() {
     return (
-        <div id="previousWork">
-            <hr />
-            {previousWorks.map((work) => {
-                return (
-                    <div key={work.role}>
-                        <br />
-                        <p>Role: {work.role}</p>
-                        <p>Company: {work.company}</p>
-                        <p>Start: {work.start}</p>
-                        <p>End: {work.end}</p>
-                        <p>Location: {work.location}</p>
-                        <p>Description: {work.description}</p>
-                        <br />
-                    </div>
-                )
-            })}
+        <div>
+            <h1>Previous Works:</h1>
+            <div id="previousWork" className="p-1 mt-0 mb-0 md:mt-2 md:mb-2 mx-auto bg-zinc-900 bg-opacity-60">
+            
+                {previousWorks.map((work) => {
+                    return (
+                        <div key={work.role} className="sm:block rounded-lg bg-white/90 px-4 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur bg-zinc-900/90 text-zinc-200 ring-white/10 m-2">
+                            <div className="flex justify-between w-full p-2 mt-0 mb-0 md:mt-2 md:mb-0 mx-auto bg-zinc-900 bg-opacity-60">
+                                <div>
+                                    <h3>{work.role}@ <b>{work.company}</b></h3>
+                                </div>
+                                <div>
+                                    <h3>{work.start} - {work.end}</h3>
+                                </div>
+                            </div>
+                            <p className="pl-2 pr-2 mt-0 mb-0 md:mt-0 md:mb-2 mx-auto bg-zinc-900 bg-opacity-60">Location: {work.location}</p>
+                            <p className="pl-2 pr-2 pb-4 mt-0 mb-0 md:mt-0 md:mb-2 mx-auto bg-zinc-900 bg-opacity-60">Description: {work.description}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
--- src/data/previousWork.ts
+++ src/data/previousWork.ts
@@ -4,9 +4,9 @@ export const previousWorks:PreviousWork[] = [
     {
         role: "Coding Expert",
         company: "Remotask",
-        start: "1 Sep 2023",
-        end: "1 Feb 2024",
-        description: "Enhanced Code Usability: Refined LLM-generated code for correctness, clarity, and alignment with user requirements, ensuring accurate and functional outputs.Evaluated and Compared LLM Responses: Assessed accuracy, relevance, and user alignment in LLM responses, comparing them to identify strengths, weaknesses, and areas for improvement in future LLM iterations.Accelerated LLM Learning: Provided detailed feedback with rewrites, explanations, and clear code examples, significantly contributing to developing more accurate and reliable LLM-generated outputs.",
+        start: "September 2023",
+        end: "February 2024",
+        description: "Worked as a Coding Expert at Remotask, reviewing and writing code in languages like Python, JavaScript, and C++. Contributed to AI training by debugging, optimizing algorithms, and evaluating code prompts to improve machine learning models. Strengthened coding accuracy, problem-solving, and multi-language development skills.",
         location:"Remote",
     },
     {
@@ -14,7 +14,7 @@ export const previousWorks:PreviousWork[] = [
         company: "The Sparks Foundation",
         start: "September 2022",
         end: "October 2022",
-        description: "Completed a 4-week Data Science and Business Analytics internship at The Sparks Foundation.Completed 6 tasks focused on data analysis, machine learning, and business analytics.Utilised programming languages such as Python, R, and Tableau for data analysis, visualization, and business analytics.Conducted linear regression, unsupervised learning, decision tree algorithms, and hybrid modeling for stock price prediction.Developed data analysis, machine learning, and business analytics skills, providing valuable experience for future roles in these areas.",
+        description: "Completed a Data Science and Business Analytics internship at The Sparks Foundation, working on 6 tasks involving data analysis and machine learning. Used Python, R, and Tableau for visualization and applied techniques like linear regression, clustering, and hybrid modeling for stock prediction.",
         location:"Remote",
     }
 ]
